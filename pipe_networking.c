3ab31c83ece50dd65bc669ebdaf0cc815bf63ec6
Jake Zaia <jzaia@stuy.edu>
2017-12-13 15:36:14 -0500

    Temp push (CODE NOT WORKING)

--- pipe_networking.c
+++ pipe_networking.c
@@ -5,13 +5,26 @@
   server_handshake
   args: int * to_client
 
-  Perofrms the client side pipe 3 way handshake.
+  Performs the client side pipe 3 way handshake.
   Sets *to_client to the file descriptor to the downstream pipe.
 
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_handshake(int *to_client) {
-  return 0;
+  char buffer[BUFFER_SIZE];
+
+
+  open(CLIENTNAME, )
+
+  if (! mkfifo(CLIENTNAME, 0644))
+    *to_client = open(CLIENTNAME, O_RDONLY);
+  else
+    printf("ERROR: %s\n", strerror(errno));
+
+  if (! mkfifo(CLIENTNAME, 0644))
+    return open(SERVERNAME, O_RDONLY);
+  printf("ERROR: %s\n", strerror(errno));
+  return -1;
 }
 
 
@@ -19,7 +32,7 @@ int server_handshake(int *to_client) {
   client_handshake
   args: int * to_server
 
-  Perofrms the client side pipe 3 way handshake.
+  Performs the client side pipe 3 way handshake.
   Sets *to_server to the file descriptor for the upstream pipe.
 
   returns the file descriptor for the downstream pipe.
